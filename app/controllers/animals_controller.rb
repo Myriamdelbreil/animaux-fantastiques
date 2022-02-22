6f9108bcafab1a9950a0ffa23237fdebd37dff8f
Thomas Voisin <thomas.voisin08@gmail.com>
2022-02-22 11:51:37 +0100

    setup pundit okay for animals & bookings

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,19 +1,24 @@
 class AnimalsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
-    @animals = Animal.all
+    @animals = policy_scope(Animal).order(created_at: :desc)
   end
 
   def show
     @animal = Animal.find(params[:id])
+    authorize @animal
   end
 
   def new
     @animal = Animal.new
+    authorize @animal
   end
 
   def create
     @animal = Animal.new(animal_params)
     @animal.user = current_user
+    authorize @animal
     if @animal.save
       redirect_to animal_path(@animal)
     else
@@ -23,17 +28,20 @@ class AnimalsController < ApplicationController
 
   def edit
     @animal = Animal.find(params[:id])
+    authorize @animal
   end
 
   def update
     @animal = Animal.find(params[:id])
     @animal.update(animal_params)
+    authorize @animal
     redirect_to animal_path(@animal)
   end
 
   def destroy
     @animal = Animal.find(params[:id])
     @animal.destroy
+    authorize @animal
     redirect_to animals_path
   end
 
--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,21 +1,23 @@
 class BookingsController < ApplicationController
-
   def index
-    @bookings = Booking.all
+    @bookings = policy_scope(Booking).order(created_at: :desc)
   end
 
   def show
     @booking = Booking.find(params[:id])
+    authorize @booking
   end
 
   def new
     @booking = Booking.new(params[:id])
+    authorize @booking
   end
 
   def create
     @booking = Booking.new(booking_params)
     @booking.user = current_user
     @booking.animal = animal(params[:id])
+    authorize @booking
     if @booking.save
       redirect_to booking_path()
     else
