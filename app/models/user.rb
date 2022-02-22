413f780ba1bd1a5543e6458a9f4ae0f0dbb31bd2
Myriam Delbreil <myriam.delbreil@live.fr>
2022-02-22 15:09:06 +0100

    validation photo et ajout photo dans le form user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :username, length: { minimum: 5 }
+  validates :photo, presence: true
   has_many :animals
   has_many :bookings
 end
