668c42e1bcdb27f418a17e2c7dedbb263144247b
louise ouldhaddi <ouldhaddi@gmail.com>
2022-02-22 10:45:57 +0100

    models animals + schema" "

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -2,6 +2,6 @@ class Animal < ApplicationRecord
   belongs_to :user
   has_many :bookings
   validates :name, :description, :price, :category, presence: true
-  validates :category, inclusion: {in: ["Acromentule", "Augurey", "Botruc", "Centaure", "Dragon", "Farfadet", "Hippogriffe", "Licorne", "Loup-Garou", "Phénix", "Troll"]}
+  validates :category, inclusion: { in: ["Acromentule", "Augurey", "Botruc", "Centaure", "Dragon", "Farfadet", "Hippogriffe", "Licorne", "Loup-Garou", "Phénix", "Troll"] }
   has_one_attached :photo
 end
