ebfcec075d30301842e32f13730de5c599d299af
louise ouldhaddi <ouldhaddi@gmail.com>
2022-02-22 14:12:44 +0100

    variable constante CATEGORIES

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,7 +1,8 @@
 class Animal < ApplicationRecord
+  CATEGORIES = ["Acromentule", "Augurey", "Botruc", "Centaure", "Dragon", "Farfadet", "Hippogriffe", "Licorne", "Loup-Garou", "Phénix", "Troll"]
   belongs_to :user
   has_many :bookings, dependent: :destroy
   validates :name, :description, :price, :category, presence: true
-  validates :category, inclusion: { in: ["Acromentule", "Augurey", "Botruc", "Centaure", "Dragon", "Farfadet", "Hippogriffe", "Licorne", "Loup-Garou", "Phénix", "Troll"] }
+  validates :category, inclusion: { in: CATEGORIES }
   has_one_attached :photo
 end
